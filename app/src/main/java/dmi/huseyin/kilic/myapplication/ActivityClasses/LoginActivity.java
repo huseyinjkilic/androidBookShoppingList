006529f40a6d360559377328f6fd6f3e5725b87b
Huseyin KILIC <kilic@netas.lab.nortel.com>
2017-10-01 21:24:42 +0300

    Fixed authentication error message

--- app/src/main/java/dmi/huseyin/kilic/myapplication/ActivityClasses/LoginActivity.java
+++ app/src/main/java/dmi/huseyin/kilic/myapplication/ActivityClasses/LoginActivity.java
@@ -2,6 +2,7 @@ package dmi.huseyin.kilic.myapplication.ActivityClasses;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -9,9 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import dmi.huseyin.kilic.myapplication.DataClasses.BookDetails;
 import dmi.huseyin.kilic.myapplication.RestDriverClasses.ApiService;
 import dmi.huseyin.kilic.myapplication.DataClasses.Book;
 import dmi.huseyin.kilic.myapplication.R;
@@ -36,42 +39,41 @@ public class LoginActivity extends AppCompatActivity  {
         mPasswordView = (EditText) findViewById(R.id.password);
         Button mButton = (Button) findViewById(R.id.user_sign_in_button);
 
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+
+
         mButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
 
-                RetroDriver retroDriver = RetroDriver.getInstance();
+                    RetroDriver retroDriver = RetroDriver.getInstance();
                     retroDriver.setPassword(mUserName.getText().toString(), mPasswordView.getText().toString());
-                OkHttpClient okHttpClientLogin = retroDriver.getOkHttpClientLogin();
-                Retrofit retrofit = retroDriver.getRetrofit(okHttpClientLogin);
-                ApiService service = retrofit.create(ApiService.class);
-                Call<List<Book>> call = service.getAllBooks();
-                call.enqueue(new Callback<List<Book>>() {
+                    OkHttpClient okHttpClientLogin = retroDriver.getOkHttpClientLogin();
+                    Retrofit retrofit = retroDriver.getRetrofit(okHttpClientLogin);
 
-                        @Override
-                        public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
-                            try {
-                                List<Book> bookData = response.body();
-                                ArrayList<Book> arrayListBookData = new ArrayList<>();
-                                for (Book books: bookData) {
-                                    arrayListBookData.add(books);
-                                }
-                                if(bookData.size() == 0) {
-                                    Toast.makeText(getApplicationContext(), "Error with authentication", Toast.LENGTH_LONG).show();
-                                } else {
-                                    Intent listInent = new Intent(getApplicationContext(), MainActivity.class);
-                                    listInent.putExtra("bookData", arrayListBookData);
-                                    startActivity(listInent);
-                                }
-                            } catch (Exception e) {
-                                Log.d("onResponse", "There is an error");
-                                e.printStackTrace();
+                    ApiService service = retrofit.create(ApiService.class);
+                    Call<List<Book>> call = service.getAllBooks();
+
+                    try {
+                        List<Book> bookData = call.execute().body();
+                        if(bookData !=null) {
+                            ArrayList<Book> arrayListBookData = new ArrayList<>();
+                            for (Book books : bookData) {
+                                arrayListBookData.add(books);
                             }
+                            Intent listInent = new Intent(getApplicationContext(), MainActivity.class);
+                            listInent.putExtra("bookData", arrayListBookData);
+                            startActivity(listInent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Authentication Error", Toast.LENGTH_LONG).show();
                         }
-                        @Override
-                        public void onFailure(Call<List<Book>> call, Throwable t) {
-                            Log.d("onFailure", t.toString());
-                        }
-                    });
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
         });
     }
